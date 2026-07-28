22f11d06fa8610b90844720ad9f8cac33319cf87
Eric Villanueva Asoy <ericasoy99@gmail.com>
2026-07-29 00:44:54 +0800

    Eric Villanueva Asoy

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleInfo(){
+
+    const info = document.getElementById("infoBox");
+
+    if(info.style.display === "block"){
+
+        info.style.display = "none";
+
+    }else{
+
+        info.style.display = "block";
+
+    }
+
+}
